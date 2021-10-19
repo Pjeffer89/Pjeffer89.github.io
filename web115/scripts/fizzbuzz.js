d0cdb97f1633774635b5c282d9b71a6eb4c1f831
Pjeffer89 <69550350+Pjeffer89@users.noreply.github.com>
2021-10-18 22:51:44 -0400

    fizzbuzz1
    fizzbuzz1

--- web115/scripts/fizzbuzz.js
+++ web115/scripts/fizzbuzz.js
@@ -1,24 +1,34 @@
 function getName() {
-let fName = document.getElementById('fName').value;
-let lName = document.getElementById('lName').value;
-let mInitial = document.getElementById('mInitial').value;
-document.getElementById('greeting').textContent = "Welcome to Jeffers Racing and Performance, " + fName + " " + mInitial + ". " + lName + ".";
-let counter = prompt(`How high do you want to count, ${fName}?`);
-fizzbuzzCounter(counter);
+    let fName = document.getElementById("fName").value;
+    let lName = document.getElementById("lName").value;
+    let mInitial = document.getElementById("mInitial").value;
+    document.getElementById("greeting").textContent =
+        "Welcome to Jeffers Racing and Performance, " +
+        fName +
+        " " +
+        mInitial +
+        ". " +
+        lName +
+        ".";
+    let counter = prompt(`How high do you want to count, ${fName}?`);
+    fizzbuzzCounter(counter);
 }
 
 function fizzbuzzCounter(count) {
-	for (let a = 0; a <= count; a++){
-   var evenOdd;
-   
-   if (a % 2 == 0) {
-   evenOdd = "The number is even.";}
-   else {
-   evenOdd = "The number is odd.";}
-   var newPageElement = document.createElement('li');
-   var newOutput = document.createTextNode(a + " Jeffers Racing -- " + evenOdd);
-   newPageElement.appendChild(newOutput);
-   var position = document.getElementsByTagName('ul')[0];
-   position.appendChild(newPageElement);
-   }
+    for (let a = 0; a <= count; a++) {
+        var evenOdd;
+
+        if (a % 2 == 0) {
+            evenOdd = "The number is even.";
+        } else {
+            evenOdd = "The number is odd.";
+        }
+        var newPageElement = document.createElement("li");
+        var newOutput = document.createTextNode(
+            a + " Jeffers Racing -- " + evenOdd
+        );
+        newPageElement.appendChild(newOutput);
+        var position = document.getElementsByTagName("ul")[0];
+        position.appendChild(newPageElement);
+    }
 }
---
+++ web115/scripts/fizzbuzz1.js
@@ -0,0 +1,36 @@
+function getName() {
+    let fName = document.getElementById("fName").value;
+    let lName = document.getElementById("lName").value;
+    let mInitial = document.getElementById("mInitial").value;
+    document.getElementById("greeting").textContent =
+        "Welcome to Jeffers Racing and Performance, " +
+        fName +
+        " " +
+        mInitial +
+        ". " +
+        lName +
+        ".";
+    newCounter();
+}
+
+function newCounter() {
+    var numLines = 140;
+    var output;
+    for (let i = 1; i <= numLines; i++) {
+        if (i % 15 === 0) {
+            output = "Skrrt Brap";
+        } else if (i % 5 === 0) {
+            output = "Brap";
+        } else if (i % 3 === 0) {
+            output = "Skrrt";
+        } else {
+            output = "Vroom";
+        }
+
+        var newElement = document.createElement("li");
+        var newOutput = document.createTextNode(i + " - " + output);
+        newElement.appendChild(newOutput);
+        var placement = document.getElementsByTagName("ul")[0];
+        placement.appendChild(newElement);
+    }
+}
