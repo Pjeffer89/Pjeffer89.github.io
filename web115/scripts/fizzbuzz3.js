4642b4ea34bd8d607f6a701901bef4e721944d57
Pjeffer89 <69550350+Pjeffer89@users.noreply.github.com>
2021-12-06 20:51:59 -0500

    fb3
    fb3

---
+++ web115/scripts/fizzbuzz3.js
@@ -0,0 +1,57 @@
+function getName() 
+{
+    var fName = document.getElementById("fName").value;
+    var lName = document.getElementById("lName").value;
+    var mInitial = document.getElementById("mInitial").value;
+    document.getElementById("greeting").textContent =
+        "Welcome to Jeffers Racing and Performance, " +
+        fName +
+        " " +
+        mInitial +
+        ". " +
+        lName +
+        ".";
+    newCounter();
+}
+
+function newCounter() 
+{
+    var numLines = 140;
+    var output = '';
+    var firstDivisor = 3;
+    var secondDivisor = 5;
+    var thirdDivisor = 7;
+    for (var i = 1; i <= numLines; i++) 
+    {
+        if (isDivisible(i, firstDivisor)) 
+        {
+            output = output.concat("Skrrt");
+        } else if (isDivisible(i, secondDivisor)) 
+        {
+            output = output.concat("Brap");
+        } else if (isDivisible(i, thirdDivisor)) 
+        {
+            output = output.concat("BANG!");
+        } else 
+        {
+            output = "Vroom";
+        }
+
+        var newElement = document.createElement("li");
+        var newOutput = document.createTextNode(i + " - " + output);
+        newElement.appendChild(newOutput);
+        var placement = document.getElementsByTagName("ul")[0];
+        placement.appendChild(newElement);
+    }
+}
+
+function isDivisible(num1, num2) 
+{
+    if (num1 % num2 === 0) 
+    {
+        return true;
+    } else 
+    {
+        return false;
+    }
+}
