f96eead3b8c6b37eebe64e1ba9e0a07060a1453e
Pjeffer89 <69550350+Pjeffer89@users.noreply.github.com>
2022-04-20 21:40:40 -0400

    edits

--- web215/scripts/introductionform.js
+++ web215/scripts/introductionform.js
@@ -1,30 +1,30 @@
 // Called when form is submitted.
 function formSubmit() 
 {
-    // Variables to hold form data.  Formatted to include headings.
-    let firstName = "First Name: " + document.getElementById('firstName').value;
-    let lastName = "Last Name: " + document.getElementById('lastName').value;
-    let personalBackground = "Personal Background: " + document.getElementById('personalBackground').value;
-    let professionalBackground = "Professional Background: " + document.getElementById('professionalBackground').value;
-    let academicBackground = "Academic Background: " + document.getElementById('academicBackground').value;
-    let subjectBackground = "Background in this Subject: " + document.getElementById('subjectBackground').value;
-    let computerPlatform = "Primary Computer Platform: " + document.getElementById('computerPlatform').value;
-    let courses = "Courses I'm Taking, Why: " + document.getElementById('courses').value;
-    let funnyInteresting = "Funny/Interesting item about yourself: " + document.getElementById('funnyInteresting').value;
-    let alsoShare = "I'd Also Like To Share: " + document.getElementById('alsoShare').value;
-    let codeLanguages = document.querySelectorAll('input[name="codeLanguages"]:checked');
-    let graduationYear = document.querySelector('input[name="graduationYear"]:checked');
-    let languages = [];
-    let outputDetails = [firstName, lastName, personalBackground, professionalBackground, academicBackground, subjectBackground, computerPlatform, courses, funnyInteresting, alsoShare, codeLanguages, graduationYear];
+    // Variables to hold textarea form data.  Formatted to include headings.
+    var firstName = "First Name: " + document.getElementById('firstName').value;
+    var lastName = "Last Name: " + document.getElementById('lastName').value;
+    var personalBackground = "Personal Background: " + document.getElementById('personalBackground').value;
+    var professionalBackground = "Professional Background: " + document.getElementById('professionalBackground').value;
+    var academicBackground = "Academic Background: " + document.getElementById('academicBackground').value;
+    var subjectBackground = "Background in this Subject: " + document.getElementById('subjectBackground').value;
+    var computerPlatform = "Primary Computer Platform: " + document.getElementById('computerPlatform').value;
+    var courses = "Courses I'm Taking, Why: " + document.getElementById('courses').value;
+    var funnyInteresting = "Funny/Interesting item about yourself: " + document.getElementById('funnyInteresting').value;
+    var alsoShare = "I'd Also Like To Share: " + document.getElementById('alsoShare').value;
+    var graduationYear = document.querySelector('input[name="graduationYear"]:checked');
+    
+    // To pass to introOutput().
+    var outputDetails = [firstName, lastName, personalBackground, professionalBackground, academicBackground, subjectBackground, computerPlatform, courses, funnyInteresting, alsoShare, outputLanguages, graduationYear];
 
     // Checkboxes
-    codeLanguages.forEach((checkbox) => 
-    {
-        languages.push(checkbox.value)
-    });
+    var codeLanguages = document.querySelectorAll('input[name="codeLanguages"]:checked');
+    var languages = [];
+    codeLanguages.forEach((checkbox) => {languages.push(checkbox.value)});
+    var outputLanguages = ("Code Languages: " + languages);
 
     // Radio
-    let graduation = ('Graduating: ' + graduationYear.value);
+    var graduation = ('Graduating: ' + graduationYear.value);
     
     // Call function to display form data.
     introOutput(outputDetails);
@@ -37,12 +37,12 @@ function introOutput(outputDetails)
     clearDiv = document.querySelectorAll('li');
     clearDiv.forEach((li) => li.remove());
     // Iterate through form variables, creating and appending elements for each.
-    for (let i = 0; i < outputDetails.length; i++) 
+    for (var i = 0; i < outputDetails.length; i++) 
     {
-        let element = document.createElement('li');
-        let content = document.createTextNode(outputDetails[i]);
+        var element = document.createElement('li');
+        var content = document.createTextNode(outputDetails[i]);
         element.appendChild(content);
-        let display = document.getElementById('formOutput');
+        var display = document.getElementById('formOutput');
         display.appendChild(element);
     }
 }
